6b1fa447de489a60d27cec0891f4a17e40aca5d8
Felix Jankowski <github@jankowski.me>
2012-08-13 20:50:00 -0400

    Initial Commit

---
+++ OpenMensa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenMensa
+//
+//  Created by Felix Jankowski on 12.06.12.
+//  Copyright (c) 2012 openmensa.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ OpenMensa/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  OpenMensa
+//
+//  Created by Felix Jankowski on 12.06.12.
+//  Copyright (c) 2012 openmensa.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
